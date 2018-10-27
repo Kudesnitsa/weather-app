5fdd1f3ef0d46ac1f61dc91816d044c92492e882
Diana <dl11021997@gmail.com>
2018-10-27 15:33:16 +0300

    fix mob version

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,26 @@
-jQuery(document).ready(function() {
-    jQuery('.container').carousel({
-        num: 5,
-        maxWidth: 400,
-        maxHeight: 660,
-        distance: 100,
-        scale: 0.9,
-        animationTime: 1000,
-        showTime: 4000
-    });
+jQuery(document).ready(function () {
+    if (jQuery(document).width() > 540) {
+        console.log(jQuery(document).width() );
+        jQuery('.container').carousel({
+            num: 3,
+            maxWidth: 400,
+            maxHeight: 660,
+            distance: 100,
+            scale: 0.9,
+            animationTime: 1000,
+            showTime: 4000
+        });
+    }
+    else {
+        console.log(jQuery(document).width());
+        jQuery('.container').carousel({
+            num: 3,
+            maxWidth: 300,
+            maxHeight: 450,
+            distance: 100,
+            scale: 0.9,
+            animationTime: 1000,
+            showTime: 4000
+        });
+    }
 });
